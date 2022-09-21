e6fd9ebf3498ae615aa9de910d9fbeb31e9425ee
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-09-21 23:07:44 +0630

    game template

--- disgame.py
+++ disgame.py
@@ -1,2 +1,137 @@
-def display():
-    print('displing...')
+import turtle
+import os
+
+score_a = 0
+score_b = 0
+
+wn = turtle.Screen()
+wn.title("pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score _a
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("orange")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("green")
+ball.penup()
+ball.goto(0, 0)
+# move by 2px
+ball.dx = -2
+ball.dy = -2
+
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("player A : {} Player B: {}".format(score_a, score_b),
+          align="center", font=("Courier", 24, "normal"))
+
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    if y >= 250:
+        paddle_a.sety(250)
+    else:
+        paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    if y <= -250:
+        paddle_a.sety(-250)
+    else:
+        paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    if y >= 250:
+        paddle_b.sety(250)
+    else:
+        paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    if y <= -250:
+        paddle_b.sety(-250)
+    else:
+        paddle_b.sety(y)
+
+
+# keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "f")
+wn.onkeypress(paddle_a_down, "d")
+wn.onkeypress(paddle_b_up, "j")
+wn.onkeypress(paddle_b_down, "k")
+
+
+while True:
+    wn.update()
+
+    # move the ball - initialize the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        os.system("afplay bounce.wav&")
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() < -390:
+        ball.setx(-390)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("player A : {} Player B: {}".format(score_a, score_b),
+                  align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() > 390:
+        ball.setx(390)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("player A : {} Player B: {}".format(score_a, score_b),
+                  align="center", font=("Courier", 24, "normal"))
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor()+50 and ball.ycor() >= paddle_b.ycor()-50):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor()+50 and ball.ycor() >= paddle_a.ycor()-50):
+        ball.setx(-340)
+        ball.dx *= -1
