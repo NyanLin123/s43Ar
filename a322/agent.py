c77102c34b2e71c081563bf84903aa7beb346935
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-09-23 13:53:49 +0630

    upgrade agent.py

--- a322/agent.py
+++ a322/agent.py
@@ -6,3 +6,13 @@ class Agent(object):
     def go(self, n):
         # acts for n time setps
         raise NotImplementedError("go")
+
+
+class TP_env(object):
+    def __init__(self):
+        self.time = 0
+        self.stock = 20
+
+    def do(self, action):
+
+        return {'price': price, 'instock': self.stock}
