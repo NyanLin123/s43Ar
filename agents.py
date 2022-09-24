33009198b7a9b1207943a1edc911ffa26934cbd7
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-09-24 18:50:43 +0630

    adding hello

--- agents.py
+++ agents.py
@@ -1,4 +1,4 @@
-class TP_env(object):
+class TP_env:
 
     prices = [322, 882, 2, 291, 882, 928, 201]
     max_price_addon = 20
@@ -6,7 +6,31 @@ class TP_env(object):
     def __init__(self):
         self.time = 0
         self.stock = 20
+        self.stock_history = []
+        self.price_history = []
 
-    def do(self, action):
+    def initial_percepts(self):
+        # return initial percepts
+        self.stock_history.append(self.stock)
+        print(self.prices[0])
 
-        return {'price': price, 'instock': self.stock}
+    def do():
+        return 'hello'
+
+
+class TP_agent(Agent):
+    def __init__(self, env):
+        self.env = env
+        self.spent = 0
+        precetps = env.initial_percetps()
+        self.ave = self.last_price = precepts['price']
+        self.instock = precetps['instock']
+
+    def go(self, n):
+        # go for n time steps
+        for i in range(n):
+            if self.last_price < 0.9*self.ave and self.instock < 60:
+                tobuy = 48
+
+
+print('hello')
